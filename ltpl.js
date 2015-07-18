66828fb548915f93fd89d2e526fa13851be977ad
Tsotsi <tsotsi@tsotsi.cn>
2015-07-18 21:11:26 +0800

    start ltpl

--- ltpl.js
+++ ltpl.js
@@ -1,3 +1,12 @@
 /**
  * Created by Tsotsi on 2015/7/17.
  */
+;(function(){
+    "use strict";
+ var Tpl=function(){};
+
+var ltpl=function(tpl,data,type){
+    var type=type||0;
+};
+    window.ltpl=ltpl;
+})();
